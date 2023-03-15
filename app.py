810c82ed86fd1ce73ff8026f043850cb90a3b604
Sahil-987 <sahil83765@gmail.com>
2023-03-15 11:55:26 +0530

    added-extract-features

--- app.py
+++ app.py
@@ -15,6 +15,15 @@ def model():
     print(model.summary())
     return model
 
+def extract_features(img_path,model):
+    img = image.load_img(img_path,target_size=(224,224))
+    img_array = image.img_to_array(img)
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img_array)
+    result = model.predict(preprocessed_img).flatten()
+    normalized_result = result / norm(result)
+    return normalized_result
+
 
 if __name__ == "__main__":
     model = model()
